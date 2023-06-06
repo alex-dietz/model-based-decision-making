2c59e8dd6f7b759b619714d33aca14d282e33bd8
arielgoldin <arielgoldinm@gmail.com>
2023-06-06 13:56:14 +0200

    Update on policies
    Reduce to on planning step, update policies

--- final assignment/funs_viz.py
+++ final assignment/funs_viz.py
@@ -10,28 +10,16 @@ import pandas as pd
 
 
 
-def tidy_results_formulation3(results):
+
+
+def tidy_results(results,formulation,planning_steps):
     """Creates and returns a data frame that:
          1) Includes the information of the scenarios and outcomes
          2) Creates a new column with the Dike ring number 
          and melts all the columns that have different values for each Dike ring
        """
 
-    
-
-
-
-    return melted_df
-
-
-def tidy_results(results,formulation):
-    """Creates and returns a data frame that:
-         1) Includes the information of the scenarios and outcomes
-         2) Creates a new column with the Dike ring number 
-         and melts all the columns that have different values for each Dike ring
-       """
-
-    if formulation == 6:
+    if formulation == 6 and planning_steps == 3:
         experiments, outcomes = results
         
         results_df = pd.concat([pd.DataFrame(experiments),pd.DataFrame(outcomes)], axis=1)
@@ -67,7 +55,7 @@ def tidy_results(results,formulation):
             melted_df = pd.concat([melted_df,melted_df_n],axis=1)
     
     
-    elif formulation == 3:
+    elif formulation == 3 and planning_steps== 3:
         experiments, outcomes = results
         
         results_df = pd.concat([pd.DataFrame(experiments),pd.DataFrame(outcomes)], axis=1)
@@ -101,9 +89,42 @@ def tidy_results(results,formulation):
             melted_df_n=melted_df_n.drop(["Dike ring"],axis=1)
             melted_df = pd.concat([melted_df,melted_df_n],axis=1)
 
+    
+
+    elif formulation == 6 and planning_steps == 1:
+        experiments, outcomes = results
+        
+        results_df = pd.concat([pd.DataFrame(experiments),pd.DataFrame(outcomes)], axis=1)
+
+        cols_to_melt=column_dict = {
+        'Bmax': ['A.1_Bmax', 'A.2_Bmax', 'A.3_Bmax', 'A.4_Bmax', 'A.5_Bmax'],
+        'Brate': ['A.1_Brate', 'A.2_Brate', 'A.3_Brate', 'A.4_Brate', 'A.5_Brate'],
+        'pfail': ['A.1_pfail', 'A.2_pfail', 'A.3_pfail', 'A.4_pfail', 'A.5_pfail'],
+        'RfR 0': ['0_RfR 0', '1_RfR 0', '2_RfR 0', '3_RfR 0', '4_RfR 0'],
+        'DikeIncrease 0': ['A.1_DikeIncrease 0', 'A.2_DikeIncrease 0', 'A.3_DikeIncrease 0', 'A.4_DikeIncrease 0', 'A.5_DikeIncrease 0'],
+        'Dike Investment Costs': ['A.1_Dike Investment Costs', 'A.2_Dike Investment Costs', 'A.3_Dike Investment Costs', 'A.4_Dike Investment Costs', 'A.5_Dike Investment Costs'],
+        'Expected Annual Damage': ['A.1_Expected Annual Damage', 'A.2_Expected Annual Damage', 'A.3_Expected Annual Damage', 'A.4_Expected Annual Damage', 'A.5_Expected Annual Damage'],
+        'Expected Number of Deaths': ['A.1_Expected Number of Deaths', 'A.2_Expected Number of Deaths', 'A.3_Expected Number of Deaths', 'A.4_Expected Number of Deaths', 'A.5_Expected Number of Deaths']
+        }
+
+        melted_df=pd.melt(results_df, id_vars=['A.0_ID flood wave shape', 'discount rate 0','EWS_DaysToThreat', 'scenario', 'policy', 'model', 'RfR Total Costs', 'Expected Evacuation Costs'], 
+                        value_vars=cols_to_melt["Bmax"],
+                        value_name="Bmax", 
+                        var_name="Dike ring"
+            )
+
+        melted_df["Dike ring"]=melted_df['Dike ring'].str[:3]
+        melted_df=melted_df.drop(["Bmax"],axis=1)
+
+        for n in cols_to_melt:
+            melted_df_n=pd.melt(results_df, id_vars=[], value_vars=	cols_to_melt[n],value_name=n, var_name="Dike ring"
+            )
+            melted_df_n=melted_df_n.drop(["Dike ring"],axis=1)
+            melted_df = pd.concat([melted_df,melted_df_n],axis=1)
+    
+    
     return melted_df
 
-#'Total costs': ['A.1 Total Costs', 'A.2 Total Costs', 'A.3 Total Costs', 'A.4 Total Costs', 'A.5 Total Costs'],
 
 
 def merge_results(results):
